78967faf39c837e3069ec73392e8796c43d409a0
drite121 <rudidarmawan00@gmail.com>
2024-05-21 15:05:09 +0700

    update

--- resources/views/DadJokes.blade.php
+++ resources/views/DadJokes.blade.php
@@ -1,35 +1,35 @@
 @extends('layouts.app')
 @section('content')
     <script>
-       fetch('https://www.google.com/finance/quote/UNVR:IDX',{
-            header:{
-                'Accept': 'application/json'
-            }
-       }).then(function(response){
+        async function fetchJoke() {
+            const response = await fetch("http://icanhazdadjoke.com", {
+            headers: {Accept: "application/json",},
+            });
             return response.json();
-       }).then(function(data){
-            console.log(data)
-       });
-       
+        }
+
+        async function Generate() {
+            const { joke } = await fetchJoke();
+            document.getElementById("joke").innerHTML = joke;
+        }
     </script>
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-sm-8">
                 <div class="bg-white border">
                     <div class="card-body">
-                        <h2 class="text-center">I am Thinking of a number Between 1 - 100</h2>
-                        <h2 class="text-center">Can You Guess it</h2>
+                        <h2 class="text-center">Dad Jokes</h2>
                         <br>
                         <div class="align-items-center">
                             <div class="form-row align-items-center">
                                 <div class="col-md-3">                                
                                 </div>
                                 <div class="col-md-6">
-                                    <input type="number"  class="form-control" id="Number" min=1 max=100>
+                                    <p id="joke"></p>
                                     <br>
-                                    <button type="button" class="btn btn-primary btn-block" onclick="Guess()">Guess</button>
+                                    <button type="button" class="btn btn-primary btn-block" onclick="Generate()">Get Joke</button>
                                     <br>
-                                    <p>Guess Number <label id="Low">1</label> - <label id="High">100</label></p>
+                                    
                                 </div>
                                 <div class="col-md-3">
                                 </div>
