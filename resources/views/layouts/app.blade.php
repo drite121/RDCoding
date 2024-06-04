a12451a91b40472849fade4243b46b9105bb19b1
drite121 <rudidarmawan00@gmail.com>
2024-06-04 17:32:09 +0700

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="" type="image/gif" sizes="16x16">
+    <link rel="icon" href="favicon.png" type="image/gif" sizes="16x16">
     <title style="font-size: 20px">{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
